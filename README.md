db2048f98b512837e1f6faf3f9619f94b8aed499
aturx <pywizard6261@gmail.com>
2021-06-30 18:39:53 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# try-todo-idx
+# DID 实例
+
+尝试DID身份的解决方案
+
+# 实现
+
+- web3 UI 
+- DID
+- web3 wallet
+
+# 尝试使用:
+
+- https://ceramic.network/
+- https://3boxlabs.com/
+
+# 使用
+
+`
+yarn 
+
+yarn start
+`
---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { MetaMaskButton, Text }from 'rimble-ui'
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { EthereumAuthProvider, ThreeIdConnect } from '@3id/connect'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import Ceramic from '@ceramicnetwork/http-client'
+import { IDX } from '@ceramicstudio/idx'
+import { DID } from 'dids'
+
+
+function App() {
+
+  const [yourDID, setYourDID] = useState(null);
+  const [yourAddress, setYourAddress] = useState(null);
+  
+
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider, // required
+      options: {
+        infuraId: "458466a9f95e42a8a35fe18a94b2a475" // required
+      }
+    }
+  };
+
+  // ethereum 
+  const web3Modal = new Web3Modal({
+    network: "mainnet", // optional
+    cacheProvider: true, // optional
+    providerOptions // required
+  });
+  
+ 
+  // did  ceramic
+  const threeIdConnect = new ThreeIdConnect()
+  
+  const authenticate = async () => {
+    const ethProvider = await web3Modal.connect()
+    const addresses = await ethProvider.enable()
+    setYourAddress(addresses[0])
+    const authProvider = new EthereumAuthProvider(ethProvider, addresses[0])
+    await threeIdConnect.connect(authProvider)
+  
+    const ceramic = new Ceramic('https://ceramic-clay.3boxlabs.com')
+    const did = new DID({
+      provider: threeIdConnect.getDidProvider(),
+      resolver: ThreeIdResolver.getResolver(ceramic)
+    })
+  
+    await did.authenticate()
+    console.log(did.id)
+    setYourDID(did.id)
+  
+    const jws = await did.createJWS({ hello: 'world' })
+    console.log(jws)
+  
+    window.idx = new IDX({ ceramic })
+    window.ceramic = ceramic
+    window.did = did.id
+  }
+  
+ 
+  return (
+ 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <MetaMaskButton size="large" onClick={authenticate}>Click me!</MetaMaskButton>
+          
+          
+        </p> 
+        <h3>
+        <Text>你的DID： {yourDID}</Text>
+        </h3> 
+        <h3>
+        <Text>你的地址：{yourAddress}</Text>
+        </h3>   
+      </header>
+      </div>
+   
+  );
+}
+
+export default App;
